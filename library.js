b49be58554add061d0b1177d7041ae30cb54e7db
Ned27 <eng27@students.stab.org>
2025-01-15 10:12:09 -0500

    Add files via upload

--- library.js
+++ library.js
@@ -23,7 +23,9 @@ var longitudes= getColumn(url,7)
 var elevations= getColumn(url,8)
 var dominantRockTypes= getColumn(url,9)
 var tectonicSetting= getColumn(url,10)
-
+// This function takes the user's desired region and returns the number of Volcanoes in that region
+// region (string) - takes the user's desired region of the world
+// sum (number) - returns the number of volcanoes in region
 function numberInRegion(region){
 sum=0;
     for(var i=0;i<volcanoNames.length;i++){
@@ -37,7 +39,9 @@ sum=0;
     return sum
 }
 console.log(numberInRegion("Italy"))
-
+// This function takes a desired rock type and returns all Volcanoes made of that rock type
+// rocktype (string) - Takes the user's desired rock type
+// matchingOutputs (list) - Returns the all volcanoes made of that rock type 
 function findVolcanoes(rocktype){
 var matchingOutputs=[];
     for(var i=0; i<volcanoNames.length;i++){
@@ -51,7 +55,9 @@ var matchingOutputs=[];
     return matchingOutputs
 }
 console.log(findVolcanoes("foidite"))
-
+// This Function takes a country from the user and finds the volcanoe with the most recent eruption
+// country (string) - takes the desired country of the user
+// match (single answer) - returns the volcanoe with the most recent erupption in that country
 function lastEruption(country){
 var max=-100000000;
 var match="";
@@ -71,7 +77,9 @@ var match="";
 
 console.log(lastEruption("Italy"))
 
-
+// This function finds all the volcanoes above the user's desired elevation
+// feet (number) - takes the desired elevation of the user
+// tallVolcanoes (list) - returns all volcanoes above user's desired elevation
 function searchElevationAbove(feet){
     var tallVolcanoes = [];
     feet = Math.round(feet)
@@ -84,7 +92,9 @@ function searchElevationAbove(feet){
 return tallVolcanoes
 }
 console.log(searchElevationAbove(-2000))
-
+// This function finds the volcanoe closest to user's desired location
+// Longitude/latitude (number) - takes user's desire coordinates
+// closestVolcanoe (single answer) - returns volcanoe closest to user's coordinates
 function findLocationClosest(longitude, latitude){
 
     var shortestDistance = 1000000000000000;
