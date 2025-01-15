e31a55ba58b5b236abbe0bccd5168e3612a0d085
tylerk42 <tkerrigan28@students.stab.org>
2025-01-14 20:21:03 -0500

    Add files via upload

--- library.js
+++ library.js
@@ -3,7 +3,20 @@ var url ="https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Sc
 var volcanoNames= getColumn(url,1)
 var countries= getColumn(url,2)
 var types= getColumn(url,3)
-var lastKnownEruptions= getColumn(url,4)
+var oldLastKnownEruptions= getColumn(url,4)
+var newLastKnownEruptions=[];
+    for(var i=0; i<oldLastKnownEruptions.length;i++){
+        if(oldLastKnownEruptions[i].includes("BCE")){
+            newLastKnownEruptions.push(parseFloat(oldLastKnownEruptions[i])*-1)
+        }
+        else{
+            newLastKnownEruptions.push(parseFloat(oldLastKnownEruptions[i]))
+        }
+    }
+
+
+
+
 var regions= getColumn(url,5)
 var latitudes= getColumn(url,6)
 var longitudes= getColumn(url,7)
@@ -18,10 +31,48 @@ sum=0;
             sum=sum+1
         }
         }
-
     if (sum==0){
         return -1
     }
     return sum
 }
-console.log(numberInRegion())
+console.log(numberInRegion("Italy"))
+
+function findVolcanoes(rocktype){
+var matchingOutputs=[];
+    for(var i=0; i<volcanoNames.length;i++){
+        if(dominantRockTypes[i].toLowerCase().includes(rocktype)){
+            matchingOutputs.push(volcanoNames[i])
+        }
+    }
+    if (matchingOutputs.length==0){
+        return "none found"
+    }
+    return matchingOutputs
+}
+console.log(findVolcanoes("foidite"))
+
+function lastEruption(country){
+var max=-100000000;
+var match="";
+    for(var i=0; i<volcanoNames.length; i++){
+        if(countries[i]==country){
+            if(max<newLastKnownEruptions[i]){
+            max=newLastKnownEruptions[i]
+            match=volcanoNames[i]
+            }
+        }
+    }
+    if(match==""){
+        return "Error"
+    }
+    return match
+}
+
+console.log(lastEruption("Italy"))
+
+
+
+
+
+
